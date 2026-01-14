19f6c35493b19ee0f46af98bfc79c76e85ccae14
Sanjith Kumar <53137451+thesanjithkumar@users.noreply.github.com>
2026-01-14 23:12:11 +0530

    Refactor research and reviewer agents for type hints
    Updated function signatures to include return type and changed verbosity to True.

--- agents.py
+++ agents.py
@@ -1,21 +1,21 @@
 from langchain_classic.agents import AgentExecutor, create_react_agent
 
 
-def research_agent(llm, search, research_prompt):
-    researcher_agent = create_react_agent(llm, [search], research_prompt)
+def research_agent(llm, search, research_prompt) -> AgentExecutor:
+    researcher_agent = create_react_agent(llm=llm, tools=[search], prompt=research_prompt)
     return AgentExecutor(
         agent=researcher_agent, 
         tools=[search], 
-        verbose=False, 
+        verbose=True, 
         handle_parsing_errors=True
     )
 
 
-def reviewer_agent(llm, review_prompt):
+def reviewer_agent(llm, review_prompt) -> AgentExecutor:
     reviewer_agent = create_react_agent(llm=llm, prompt=review_prompt, tools=[])
     return AgentExecutor(
         agent=reviewer_agent, 
         tools=[], 
-        verbose=False, 
+        verbose=True, 
         handle_parsing_errors=True
     )
